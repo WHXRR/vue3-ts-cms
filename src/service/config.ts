ba956deabe703b3044c33633e5a22cc0998ce666
WXHRR <1147232219@qq.com>
2023-09-20 16:35:41 +0800

    feat(baseurl): update baseurl

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,7 +1,7 @@
 let BASE_URL = ''
 
 if (import.meta.env.PROD) {
-  BASE_URL = 'http://47.97.159.103:6666'
+  BASE_URL = 'http://47.97.159.103:3334/api'
 } else {
   BASE_URL = 'http://codercba.com:5000'
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import topLevelAwait from "vite-plugin-top-level-await";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     topLevelAwait({
-      // The export name of top-level await promise for each chunk module
       promiseExportName: "__tla",
-      // The function to generate import names of top-level await promise in each chunk module
       promiseImportName: (i: any) => `__tla_${i}`
     })
   ],
@@ -31,5 +28,5 @@ export default defineConfig({
         }
       }
     }
-  }
+  },
 })
