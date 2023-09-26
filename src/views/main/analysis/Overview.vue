57efd4fd16a03e44876968c2828bd62bfe0edc87
WXHRR <1147232219@qq.com>
2023-09-26 15:21:43 +0800

    fix: fix file extension

--- src/views/main/analysis/Overview.vue
+++ src/views/main/analysis/Overview.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { computed } from "vue"
 import { useI18n } from "vue-i18n"
-import packageData from "@/assets/js/package.json.ts"
-import tree from "@/assets/js/tree.ts"
+import packageData from "@/assets/js/package"
+import tree from "@/assets/js/tree"
 
 const { t } = useI18n()
 const textArr1 = computed(() => {
